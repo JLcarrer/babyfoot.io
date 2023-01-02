4acea853288f3d04c0603a5c25d616245ae5cac5
JL² <jeanloup.mellion@gmail.com>
2023-01-02 12:58:50 +0100

    Publish

---
+++ index.js
@@ -0,0 +1,181 @@
+import express from 'express';
+const app = express();
+import http from 'http';
+const server = http.createServer(app);
+import * as SocketIO from 'socket.io';
+const io = new SocketIO.Server(server, {cors: {origin: '*'}});
+import * as CANNON from 'cannon-es';
+import { GLTFLoader } from 'node-three-gltf';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+let rooms = [];
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.static('build'));
+
+app.get('/babyfoot', (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.sendFile(__dirname + '/models/babyfoot.gltf');
+    console.log("Sending babyfoot model");
+});
+
+app.listen(3000, () => {
+    console.log('Express listening on port 3000');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('create', (name) => {
+        console.log("Creating room : " + name);
+        rooms.push(new Room(name, socket));
+    });
+
+    socket.on('join', (name) => {
+        console.log("Joining room : " + name);
+        rooms.forEach((room) => {
+            if(room.name == name){
+                room.blue = socket;
+                room.blue.emit('start');
+                room.red.emit('start');
+                room.start();
+            }
+        });
+    });
+
+    socket.on('updatePlayer', (data) => {
+        rooms.forEach((room) => {
+            if(room.red == socket){
+                room.blue.emit('updatePlayer', data);
+            }
+            if(room.blue == socket){
+                room.red.emit('updatePlayer', data);
+            }
+            room.updatePlayer(data);
+        });
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+server.listen(3001, () => {
+    console.log('SocketIO listening on port 3001');
+});
+
+class Room{
+    constructor(name, player){
+        this.name = name;
+        this.red = player;
+        this.world = new CANNON.World();
+        this.world.gravity.set(0, -9.82, 0);
+        this.playerStartPos = [];
+        this.playerBodies = [];
+        this.addTerrain();
+        this.addPlayers();
+    }
+
+    addTerrain(){
+        const loader = new GLTFLoader();
+        loader.load('models/collider.gltf', (gltf) => {
+            const collider = gltf.scene;
+            const vertices = collider.children[0].geometry.attributes.position.array;
+            const indices = collider.children[0].geometry.index.array;
+            const colliderShape = new CANNON.Trimesh(vertices, indices);
+            this.groundMaterial = new CANNON.Material("groundMaterial");
+            const colliderBody = new CANNON.Body({mass: 0, material: this.groundMaterial});
+            colliderShape.setScale(new CANNON.Vec3(11, 2, 6));
+            colliderBody.addShape(colliderShape);
+            colliderBody.position.x = 0;
+            colliderBody.position.y = 1.25;
+            colliderBody.position.z = 0;
+            this.world.addBody(colliderBody);
+            this.addBall();
+        });
+    }
+
+    addBall(){
+        const sphereShape = new CANNON.Sphere(0.25);
+        this.ballMaterial = new CANNON.Material("ballMaterial");
+        this.ballBody = new CANNON.Body({ mass: 1, material: this.ballMaterial });
+        this.ballBody.addShape(sphereShape);
+        this.ballBody.position.x = 0;
+        this.ballBody.position.y = 2;
+        this.ballBody.position.z = 0;
+        this.ballBody.velocity.set((Math.random() - 0.5) * 4, 0, (Math.random() - 0.5) * 4);
+        this.ballBody.linearDamping = 0.1;
+        const mat = new CANNON.ContactMaterial(this.groundMaterial, this.ballMaterial, {friction: 0.1, restitution: 0.8});
+        this.world.addContactMaterial(mat);
+        this.world.addBody(this.ballBody)
+    }
+
+    addPlayers(){
+        this.addPlayer(-10.5, 0, 5);
+        this.addPlayer(-7.5, -2, 4);
+        this.addPlayer(-7.5, 2, 4);
+        this.addPlayer(-4.5, -3, 3);
+        this.addPlayer(-4.5, 0, 3);
+        this.addPlayer(-4.5, 3, 3);
+        this.addPlayer(-1.5, -4, 2);
+        this.addPlayer(-1.5, -2, 2);
+        this.addPlayer(-1.5, 0, 2);
+        this.addPlayer(-1.5, 2, 2);
+        this.addPlayer(-1.5, 4, 2);
+        this.addPlayer(1.5, -4, 1);
+        this.addPlayer(1.5, -2, 1);
+        this.addPlayer(1.5, 0, 1);
+        this.addPlayer(1.5, 2, 1);
+        this.addPlayer(1.5, 4, 1);
+        this.addPlayer(4.5, -3, 6);
+        this.addPlayer(4.5, 0, 6);
+        this.addPlayer(4.5, 3, 6);
+        this.addPlayer(7.5, -2, 7);
+        this.addPlayer(7.5, 2, 7);
+        this.addPlayer(10.5, 0, 8);
+    }
+
+    addPlayer(x, z, player){
+        const collider = new CANNON.Box(new CANNON.Vec3(0.125, 1.25, 0.25));
+        let playerMaterial = new CANNON.Material('playerMaterial');
+        const colliderBody = new CANNON.Body({ mass: 0, material: playerMaterial });
+        colliderBody.addShape(collider);
+        colliderBody.position.x = x;
+        colliderBody.position.y = 2.5;
+        colliderBody.position.z = z;
+        this.world.addBody(colliderBody);
+        this.playerStartPos.push({z: z, player: player});
+        this.playerBodies.push(colliderBody);
+    }
+
+    runBall(){
+        this.ballBody.velocity.set((Math.random() - 0.5) * 4, 0, (Math.random() - 0.5) * 4);
+    }
+
+    start(){
+        console.log(this.name);
+        this.update();
+    }
+
+    updateBall(){
+        this.red.emit('updateBall', this.ballBody.position);
+        this.blue.emit('updateBall', this.ballBody.position);
+    }
+
+    updatePlayer(data){
+        for(let i = 0; i < this.playerStartPos.length; i++){
+            if(this.playerStartPos[i].player == data.player){
+                this.playerBodies[i].position.z = this.playerStartPos[i].z + data.position;
+                this.playerBodies[i].quaternion.setFromEuler(0, 0, data.rotation);
+            }
+        }
+    }
+
+    update(){
+        this.world.step(1/60);
+        this.updateBall();
+        setTimeout(() => this.update(), 1000/60);
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,180 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import io from 'socket.io-client';
+const socket = io('http://localhost:3001/');
+
+const createRoomButton = document.getElementById('createRoomButton');
+const joinRoomButton = document.getElementById('joinRoomButton');
+const roomNameInput = document.getElementById('roomNameInput');
+
+var scene;
+var camera;
+var renderer;
+let team;
+let current = null;
+let babyfoot = null;
+let ballMesh;
+
+const url = 'http://localhost:3000/'
+
+function initialiseThree(){
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  scene.background = new THREE.Color(0x9ab586);
+
+  camera.position.z = 40;
+  camera.position.y = 15;
+  camera.lookAt(0, 0, 0);
+
+  addBabyFoot();
+  addBall();
+  addLight();
+}
+
+function createRoom(name){
+  camera.position.z = -40;
+  team = [5, 4, 2, 6];
+  socket.emit('create', name);
+}
+
+function joinRoom(name){
+  team = [8, 7, 1, 3];
+  socket.emit('join', name);
+}
+
+socket.on('updateBall', (data) => {
+  ballMesh.position.x = data.x;
+  ballMesh.position.y = data.y;
+  ballMesh.position.z = data.z;
+});
+
+socket.on('updatePlayer', (data) => {
+  babyfoot.children[data.player].position.z = data.position;
+  babyfoot.children[data.player].rotation.z = data.rotation;
+  console.log(data);
+});
+
+socket.on('start', () => {
+  window.requestAnimationFrame(loop);
+});
+
+initialiseThree();
+render();
+
+function addBabyFoot(){
+  const loader = new GLTFLoader();
+  loader.load(url + 'babyfoot', function (gltf) {
+    babyfoot = gltf.scene;
+    babyfoot.scale.set(1, 1, 1);
+    scene.add(babyfoot);
+  });
+}
+
+function addBall(){
+  const geometry = new THREE.SphereGeometry(0.25, 8, 8);
+  const material = new THREE.MeshBasicMaterial({ color: 0xaaaaaa });
+  ballMesh = new THREE.Mesh(geometry, material);
+  ballMesh.position.y = 2;
+  scene.add(ballMesh);
+}
+            
+function addLight(){
+  const light = new THREE.PointLight(0xffffff, 1, 1000);
+  light.position.set(0, 10, 0);
+  scene.add(light);
+  
+  const ambientLight = new THREE.AmbientLight(0xf0f0f0);
+  scene.add(ambientLight);
+}
+
+function render() {
+    requestAnimationFrame(render);
+    renderer.render(scene, camera);
+}
+
+let lastRender = 0;
+
+function loop(timestamp) {
+  var ms = timestamp - lastRender;
+  var delta = ms / 1000;
+  update(delta);
+  lastRender = timestamp;
+  window.requestAnimationFrame(loop)
+}
+
+function update(delta) {
+  if(Math.abs(camera.position.z) > 7.5){
+    camera.position.z = lerp(camera.position.z, 0, 0.01) * (1 - delta);
+    camera.lookAt(0, 0, 0);
+  }
+  if(babyfoot != null){
+    gravityRotation();
+  }
+}
+
+function gravityRotation(){
+  for(let i = 0; i < 4; i++){
+    if(team[i] != current){
+      babyfoot.children[team[i]].rotation.z = lerp(babyfoot.children[team[i]].rotation.z, 0, 0.05);
+    }
+  }
+}
+
+function reset(){
+  for(let i = 0; i < 4; i++){
+    babyfoot.children[team[i]].rotation.z = 0;
+    babyfoot.children[team[i]].position.z = 0;
+  }
+}
+
+function lerp(start, end, amt){
+  return (1-amt)*start+amt*end
+}
+
+function clamp(value, min, max){
+  return Math.min(Math.max(value, min), max);
+}
+
+document.addEventListener('mousemove', (e) => {
+  if(babyfoot != null && current != null){
+    let x = e.clientX;
+    let y = e.clientY;
+    babyfoot.children[current].rotation.z = (x * 0.01 - 9.6) * (team[0] === 5 ? 0.2 : -0.2);
+    babyfoot.children[current].position.z = clamp(y * 0.01 - 2, -3.5, 3.5) * (team[0] === 5 ? 0.5 : -0.5);
+    socket.emit('updatePlayer', {
+      player: current, 
+      position: babyfoot.children[current].position.z, 
+      rotation: babyfoot.children[current].rotation.z
+    });
+  }
+});
+
+document.addEventListener('keydown', (event) => {
+  switch(event.key){
+    case 'a':
+      current = team[0];
+      break;
+    case 'z':
+      current = team[1];
+      break;
+    case 'e':
+      current = team[2];
+      break;
+    case 'r':
+      current = team[3];
+      break;
+    case 't':
+      reset();
+      break;
+  }
+});
+
+createRoomButton.addEventListener('click', () => {
+  createRoom(roomNameInput.value);
+});
+
+joinRoomButton.addEventListener('click', () => {
+  joinRoom(roomNameInput.value);
+});
